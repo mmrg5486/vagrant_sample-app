96a331eca2527c161b6beac2ef55e6f777933787
Mauro <mmrg5486@gmail.com>
2016-04-10 01:42:18 -0300

    Configuro email destino en monitor.sh

--- provisioning/files/monitor.sh
+++ provisioning/files/monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/wget "10.127.0.10/sample-app/index.php" --timeout 30 -O - 2>/dev/null | grep -q "Hello World" || echo "The site is down" | /usr/bin/mail -s "Site is down" root
+/usr/bin/wget "10.127.0.10/sample-app/index.php" --timeout 30 -O - 2>/dev/null | grep -q "Hello World" || echo "The site is down" | /usr/bin/mail -s "Site is down" guzman.braso+sample-alert@batangamedia.com
 
